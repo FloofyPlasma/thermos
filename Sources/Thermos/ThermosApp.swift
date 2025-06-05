9f23d7f98dd6c0ed03b72c63d458fc514b0124cd
FloofyPlasma <github@floofyplasma.com>
2025-06-05 00:27:10 -0400

    chore: first commit

---
+++ Sources/Thermos/ThermosApp.swift
@@ -0,0 +1,12 @@
+import SwiftCrossUI
+import DefaultBackend
+
+@main
+struct ThermosApp: App {
+    var body: some Scene {
+        WindowGroup("Thermos") {
+            Text("Hello, World!")
+                .padding()
+        }
+    }
+}
